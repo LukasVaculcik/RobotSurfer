3d9875be930ca8c7c21b76460ae93b5665205004
Lukáš Vaculčík <lukas.vaculcik@gmail.com>
2024-07-15 17:04:41 +0200

    crouching

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rigidBody;
+    [SerializeField] private Transform GFX;
     [SerializeField] private float jumpForce = 10f;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private Transform feetPosition;
     [SerializeField] private float distanceToGround = 0.25f;
     [SerializeField] private float jumpTime = 0.3f;
+    [SerializeField] private float crouchHeight = 0.5f;
     private bool isGrounded = false;
     private bool isJumping = false;
     private float jumpTimer;
@@ -18,6 +20,7 @@ public class NewBehaviourScript : MonoBehaviour
     {
         isGrounded = Physics2D.OverlapCircle(feetPosition.position, distanceToGround, groundLayer);
 
+        // Jumping
         if (isGrounded && Input.GetButtonDown("Jump")) {
             isJumping = true;
             rigidBody.velocity = Vector2.up * jumpForce;
@@ -35,6 +38,19 @@ public class NewBehaviourScript : MonoBehaviour
         if (Input.GetButtonUp("Jump")) {
             isJumping = false;
             jumpTimer = 0;
+        }
+
+        // Crouching
+        if (isGrounded && Input.GetButton("Crouch")) {
+            GFX.localScale = new Vector3(GFX.localScale.x, crouchHeight, GFX.localScale.z);
+
+            if (isJumping) {
+                GFX.localScale = new Vector3(GFX.localScale.x, 1f, GFX.localScale.z);
+            }
+        }
+
+        if (Input.GetButtonUp("Crouch")) {
+            GFX.localScale = new Vector3(GFX.localScale.x, 1f, GFX.localScale.z);
         }
     }
 }
