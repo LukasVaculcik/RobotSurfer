4851ba43cc1412071b67215c999199fd4434920e
Lukáš Vaculčík <lukas.vaculcik@gmail.com>
2024-07-17 16:40:40 +0200

    add trees spawnign , adjust gameplay

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] prefabs;
-    public float spawnTime = 2f;
+    public float spawnTimeMin = 1.5f;
+    public float spawnTimeMax = 3f;
     public float speed = 10f;
+    public bool randomizeSpawnPositionY = true;
     private float timeUntilSpawn;
+    private float spawnTime;
 
     private void Update()
     {
         if (GameManager.Instance.isPlaying) {
+            spawnTime = Random.Range(spawnTimeMin, spawnTimeMax);
             SpawnLoop();
         }
     }
@@ -30,7 +34,9 @@ public class Spawner : MonoBehaviour
     {
         GameObject obstacleToSpawn = prefabs[Random.Range(0, prefabs.Length)];
         Vector3 obstaclePosition = transform.position;
-        obstaclePosition.y = Random.Range(transform.position.y, 4);
+        if (randomizeSpawnPositionY) {
+            obstaclePosition.y = Random.Range(transform.position.y, 4);
+        }
         GameObject spawnedInstance = Instantiate(obstacleToSpawn, obstaclePosition, Quaternion.identity);
         Rigidbody2D obstacleRigidBody = spawnedInstance.GetComponent<Rigidbody2D>();
         obstacleRigidBody.velocity = Vector2.left * speed;
